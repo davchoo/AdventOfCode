bd1a05ca9270a676d92cdba0bb932bb4cdf8af58
davchoo <davchoo@users.noreply.github.com>
2019-12-01 19:52:56 -0500

    Cleaned up Day 1 2019
    Moved the fuel calculation into its own function

--- aoc_davchoo/2019/day1.py
+++ aoc_davchoo/2019/day1.py
@@ -1,18 +1,22 @@
 def process_data(data):
-    return 0
+    return map(int, data.splitlines())
+
+
+def calc_fuel_req(masses):
+    return list(filter(lambda x: x > 0, [mass // 3 - 2 for mass in masses]))
 
 
 def solve_a(data):
-    masses = map(int, data.splitlines())
-    fuel_req = [mass // 3 - 2 for mass in masses]
+    masses = process_data(data)
+    fuel_req = calc_fuel_req(masses)
     return sum(fuel_req)
 
 
 def solve_b(data):
-    masses = map(int, data.splitlines())
-    fuel_req = [mass // 3 - 2 for mass in masses]
+    masses = process_data(data)
+    fuel_req = calc_fuel_req(masses)
     total_fuel = sum(fuel_req)
     while sum(fuel_req) > 0:
-        fuel_req = list(filter(lambda x: x > 0, [mass // 3 - 2 for mass in fuel_req]))
+        fuel_req = calc_fuel_req(fuel_req)
         total_fuel += sum(fuel_req)
     return total_fuel
